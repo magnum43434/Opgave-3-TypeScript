537de58006446df1fb34ecdb263b2b7b0b7db664
Magnus Hansen <magnus131hansen@gmail.com>
2020-10-01 14:59:39 +0200

    refactor

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,16 +1,14 @@
-let stringInput: HTMLInputElement = <HTMLInputElement>document.getElementById('stringInput');
-let actionType: HTMLSelectElement = <HTMLSelectElement>document.getElementById('actionType');
 let stringOutputDiv: HTMLDivElement = <HTMLDivElement>document.getElementById('content');
 let stringActionButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById('stringActionButton');
 stringActionButton.addEventListener("click", function () {
-    let userInput = stringInput.value;
-    let userActionType = actionType.value;
-    switch (userActionType) {
+    let userInput: HTMLInputElement = <HTMLInputElement>document.getElementById('stringInput');
+    let userActionType: HTMLSelectElement = <HTMLSelectElement>document.getElementById('actionType');
+    switch (userActionType.value) {
         case "toUpperCase":
-            stringOutputDiv.innerHTML = userInput.toUpperCase();
+            stringOutputDiv.innerHTML = userInput.value.toUpperCase();
             break;
         case "toLowerCase":
-            stringOutputDiv.innerHTML = userInput.toLowerCase();
+            stringOutputDiv.innerHTML = userInput.value.toLowerCase();
             break;
     }
 });
